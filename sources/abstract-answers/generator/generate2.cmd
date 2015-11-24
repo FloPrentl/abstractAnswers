e503d0002e494f488443a0d3c64afb39de25a2c3
Florian Prentl <florian.prentl@bmw.de>
2015-11-24 10:22:12 +0100

    nomsg

--- sources/abstract-answers/generator/generate2.cmd
+++
@@ -1,2 +0,0 @@
-set CLASSPATH=D:\Portable\xalan-j_2_7_2\xalan.jar
-java org.apache.xalan.xslt.Process -IN .\abstractanswers2.xml -XSL .\modern-format\modern-format.xsl -OUT ..\..\..\httpdocs\abstract-answers\generator\modern-format\what-is-an-etf.html
